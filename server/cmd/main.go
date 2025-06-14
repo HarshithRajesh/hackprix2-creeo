8a7475c6ae03568415b8c9fcafb0645db7af6fad
harshith <harshithr2004@gmail.com>
2025-06-14 22:36:11 +0530

    Fix: CORS error

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"time"
+
 	"github.com/HarshithRajesh/creeo/internal/api"
 	"github.com/HarshithRajesh/creeo/internal/config"
 	"github.com/HarshithRajesh/creeo/internal/repository"
 	"github.com/HarshithRajesh/creeo/internal/service"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -20,6 +23,16 @@ func main() {
 	userHandler := api.NewUserHandler(userService)
 
 	router := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	config.AllowMethods = []string{"POST", "GET", "PUT", "OPTIONS"}
+	config.AllowHeaders = []string{"Origin", "Content-Type", "Authorization", "Accept", "User-Agent", "Cache-Control", "Pragma"}
+	config.ExposeHeaders = []string{"Content-Length"}
+	config.AllowCredentials = true
+	config.MaxAge = 12 * time.Hour
+
+	router.Use(cors.New(config))
+	// router.Use(cors.Default())
 	router.GET("/health", health)
 	router.POST("/profile/create", userHandler.CreateProfile)
 	router.GET("/profile", userHandler.GetProfile)
