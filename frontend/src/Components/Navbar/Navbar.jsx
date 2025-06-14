f08c09ad01f3eb2807cac90b6fa0da0acbf1bcb6
CKShetty4 <ckshetty004@gmail.com>
2025-06-14 12:20:40 +0530

    add : Component and Page Setup

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import './Navbar.css';
+import { Link, useLocation } from 'react-router-dom';
 
-const Navbar = () => {
+function Navbar() {
+  const location = useLocation();
+
+  const navClass = (path) => location.pathname === path ? 'nav-icon active' : 'nav-icon';
+
   return (
-    <div>Navbar</div>
-  )
+    <div className="navbar-container">
+      <h1>Navbar</h1>
+      <Link to="/profile" className={navClass('/profile')}>
+        <i className="bi bi-person"></i>
+      </Link>
+      <Link to="/geolocation" className={navClass('/geolocation')}>
+        <i className="bi bi-geo-alt"></i>
+      </Link>
+      <Link to="/" className={navClass('/')}>
+        <i className="bi bi-house"></i>
+      </Link>
+      <Link to="/chats" className={navClass('/chats')}>
+        <i className="bi bi-chat-dots"></i>
+      </Link>
+      <Link to="/menu" className={navClass('/menu')}>
+        <i className="bi bi-list"></i>
+      </Link>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
